b800beedff52e92e54b350973418566170680297
Asapanna Rakesh <rakeshark22@gmail.com>
2020-05-26 17:21:06 +0530

    done

--- scraper.py
+++ scraper.py
@@ -1,8 +1,82 @@
-class WebScraper:
-	pass
+import requests
+from bs4 import BeautifulSoup 
+import csv 
+import pandas as pd
+import sys, time
 
-class StockScraper(WebScraper):
-	pass
 
-class CurPairScraper(WebScraper):
+class curPairScraper():
+	
+	def __init__(self, url):
+		self.names=[]
+		self.prices=[]
+		self.changes=[]
+		self.percentChanges=[]
+		
+		self.supported_urls = ["https://finance.yahoo.com/currencies"]
+		if url in self.supported_urls:
+			self.url = url
+		else:
+			raise Exception("URL Not Supported. Currently we support only https://finance.yahoo.com/currencies")
+
+
+	def get_pandasDF(self):
+		r= requests.get(self.url)
+		data=r.text
+		soup=BeautifulSoup(data, features='lxml')
+		
+		for i in range(40, 404, 14):
+			for listing in soup.find_all('tr', attrs={'data-reactid':i}):
+				
+				for name in listing.find_all('td', attrs={'data-reactid':i+3}):
+					self.names.append(name.text)
+				for price in listing.find_all('td', attrs={'data-reactid':i+4}):
+					self.prices.append(price.text)
+				for change in listing.find_all('td', attrs={'data-reactid':i+5}):
+					self.changes.append(change.text)
+				for percentChange in listing.find_all('td', attrs={'data-reactid':i+7}):
+					self.percentChanges.append(percentChange.text)
+					
+		df = (pd.DataFrame({"names": self.names, "prices": self.prices, "changes": self.changes, "per_changes": self.percentChanges}))
+		self.names = []
+		self.prices = []
+		self.changes = []
+		self.percentChanges = []
+		return df
+	
+	
+	def obj_genrator(self):
+		
+		all_pairs = self.get_pandasDF()
+		num_rows  = len(all_pairs)
+		for row in range(num_rows): 
+			yield {
+				"row_num"    : str(row),
+				"total_rows" : str(num_rows),
+				"name"       : all_pairs.loc[row, "names"],
+				"price"      : all_pairs.loc[row, "prices"],
+				"change"     : all_pairs.loc[row, "changes"],
+				"per_change" : all_pairs.loc[row, "per_changes"]
+			}
+		
+		
+#end class
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	"""
+	#usage:
+	=======
+	
+	scraper = curPairScraper("https://finance.yahoo.com/currencies")
+	
+	while (True):
+		for pair_info in scraper.obj_genrator():
+			print(pair_info)
+	"""
 	pass
